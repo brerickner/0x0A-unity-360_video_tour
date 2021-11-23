4012d8d234614171db5aa12e58bbeea3b1d164b7
Bre <49533377+brerickner@users.noreply.github.com>
2021-11-23 13:58:35 -0600

    adding progress info button working for living room

--- Assets/Scripts/InfoButton.cs
+++ Assets/Scripts/InfoButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Info_button : MonoBehaviour
+public class InfoButton : MonoBehaviour
 {
     public GameObject Textfield;
 
